587decc60f0cd77837b37301573944b4257fadc6
wiiter7 <arthur.carneiro.santos@escola.pr.gov.br>
2026-05-29 11:07:04 -0300

    s

--- script_aluno.js
+++ script_aluno.js
@@ -13,38 +13,40 @@ function calcularCompra() {
 
     
     // REGRA 1: Validação do Cupom de Desconto
-    //O nome do cupom oficial é "PROMO10". 
+    // O nome do cupom oficial é "PROMO10". 
     // Use .toUpperCase() para garantir que funcione se o usuário digitar "promo10" ou "Promo10"
-    
-    // Faça um teste condicional (if): se o 'cupomDigitado' for igual ao cupom oficial o valor de desconto será = a 10
-     
-    // DICA: Use a estrutura -> if (variavel.toUpperCase() === "VALOR") { valor da vaiável desconto}
     
     let desconto = 0;
     
+    // ESCREVA SEU CÓDIGO AQUI:
+    if (cupomDigitado.toUpperCase() === "PROMO10") {
+        desconto = 10;
+    }
 
 
     // REGRA 2: Aplicação do Desconto no Valor do Produto
-
     // Crie uma nova variável chamada 'valorComDesconto'.
     // Ela deve guardar o resultado de uma conta matemática simples:
     // O 'valorOriginal' do produto MENOS (-) o 'desconto' que você descobriu na Regra 1.
     
     // ESCREVA SEU CÓDIGO AQUI:
+    let valorComDesconto = valorOriginal - desconto;
 
 
   
     // REGRA 3: Cálculo da Taxa de Frete
-   
     // Agora crie uma estrutura condicional completa (if / else):
     // -> SE o 'valorComDesconto' for MAIOR OU IGUAL (>=) a 100, o frete continua 0.
     // -> SENÃO (else), a variável 'frete' deve receber o valor de 15.
-    //
-    // DICA: Use a estrutura -> if ( ... ) { ... } else { ... }
+    
     let frete = 0;
     
     // ESCREVA SEU IF / ELSE AQUI:
-
+    if (valorComDesconto >= 100) {
+        frete = 0;
+    } else {
+        frete = 15;
+    }
 
    
     // REGRA 4: Total Geral da Compra
@@ -53,6 +55,7 @@ function calcularCompra() {
     // Ela deve guardar a soma (+) do 'valorComDesconto' com a taxa do 'frete'.
     
     // ESCREVA SEU CÓDIGO AQUI:
+    let totalFinal = valorComDesconto + frete;
 
 
     // =================================================================
